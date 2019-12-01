1f8ea15330994c8a2e6072165c2e4d6aac9f353e
Yuki Takei <yuki@weseek.co.jp>
2019-12-02 07:51:40 +0900

    update test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,14 +3,16 @@ import * as cp from 'child_process'
 import * as path from 'path'
 
 describe.each`
-  semver
-  ${'12.3.4'}
-  ${'12.3.4-RC'}
-`('test runs', ({semver}) => {
-  test(`with semver=${semver}`, () => {
+  semver          | suffix
+  ${'12.3.4'}     | ''
+  ${'12.3.4'}     | '-nocdn'
+  ${'12.3.4-RC'}  | '-nocdn'
+`('test runs', ({semver, suffix}) => {
+  test(`with semver='${semver}', suffix='${suffix}'`, () => {
     process.env['INPUT_SOURCE'] = 'node:12';
     process.env['INPUT_TARGET'] = 'mynode';
     process.env['INPUT_SEMVER'] = semver;
+    process.env['INPUT_SUFFIX'] = suffix;
 
     const ip = path.join(__dirname, '..', 'dist', 'index.js');
     const options: cp.ExecSyncOptions = {
