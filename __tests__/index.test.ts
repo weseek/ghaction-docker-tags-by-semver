214b3d36b1a10c07b4f0c0a780ae7fb928ac5ce4
Yuki Takei <yuki@weseek.co.jp>
2019-12-02 06:05:37 +0900

    WIP: impl

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,7 +3,10 @@ import * as cp from 'child_process'
 import * as path from 'path'
 
 test('test runs', () => {
-    process.env['INPUT_WHO-TO-GREET'] = 'World';
+    process.env['INPUT_SOURCE'] = 'node:12';
+    process.env['INPUT_TARGET'] = 'node';
+    process.env['INPUT_SEMVER'] = '12.0.0';
+
     const ip = path.join(__dirname, '..', 'dist', 'index.js');
     const options: cp.ExecSyncOptions = {
         env: process.env
--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import { exec } from '@actions/exec';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const source: string = core.getInput('source');
+    const target: string = core.getInput('target');
+    const semver: string = core.getInput('semver');
+    const suffix: string = core.getInput('suffix');
+    
+    await exec('docker', ['tag', source, `${target}:latest`]);
+
+    core.setOutput('time', new Date().toTimeString());
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
