7e506d10297df53831e75d6e3c981c7e3775da2b
Yuki Takei <yuki@weseek.co.jp>
2019-12-05 05:51:08 +0900

    ensure to set default value of additional-tags

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,21 +3,23 @@ import * as cp from 'child_process'
 import * as path from 'path'
 
 describe.each`
-  semver          | suffix
-  ${'12.3.4'}     | ${''}
-  ${'12.3.4'}     | ${'-nocdn'}
-  ${'12.3.4-RC'}  | ${'-nocdn'}
-`('test runs', ({semver, suffix}) => {
-  test(`with semver='${semver}', suffix='${suffix}'`, () => {
+  semver          | suffix       | additionalTagsStr
+  ${'12.3.4'}     | ${''}        | ${''}
+  ${'12.3.4'}     | ${'-nocdn'}  | ${''}
+  ${'12.3.4'}     | ${''}        | ${'latest,lts'}
+  ${'12.3.4-RC'}  | ${'-nocdn'}  | ${'dummy'}
+`('test runs', ({semver, suffix, additionalTagsStr}) => {
+  test(`with semver='${semver}', suffix='${suffix}', additionalTagsStr='${additionalTagsStr}'`, () => {
     process.env['INPUT_SOURCE'] = 'node:12';
     process.env['INPUT_TARGET'] = 'mynode';
     process.env['INPUT_SEMVER'] = semver;
     process.env['INPUT_SUFFIX'] = suffix;
-    process.env['INPUT_PUBLISH'] = 'false';
+    process.env['INPUT_ADDITIONAL-TAGS'] = additionalTagsStr;
+    process.env['INPUT_PUBLISH'] = null;
 
     const ip = path.join(__dirname, '..', 'dist', 'index.js');
     const options: cp.ExecSyncOptions = {
-        env: process.env
+      env: process.env
     };
 
     try {
